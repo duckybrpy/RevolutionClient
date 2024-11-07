fcbe539f9de2f95f4bb8237177f706ff431fd8ec
duckybrpy <playstart45@gmail.com>
2024-11-07 13:59:17 -0300

    teste2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RevolutionClient',
-    version='alpha-0.0.1',
+    version='0.0.1a0',
     packages=find_packages(),
     install_requires=[
         'python-socketio==5.11.4',
