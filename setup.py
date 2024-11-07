b1ac033e03bc78212daa73183110e058aef092c6
duckybrpy <playstart45@gmail.com>
2024-11-07 13:51:55 -0300

    teste2

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RevolutionClient',
+    version='alpha-0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'python-socketio==5.11.4',
+        'requests==2.32.3'
+    ],
+    description='Esta em testes',
+    author='DuckyBR',
+    author_email='',
+    url='https://github.com/duckybrpy/revolutionclient',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
